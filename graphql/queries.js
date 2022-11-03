aaac8a7ddec7911b9959439d008f792ecfce5033
Ryan J Carmody <ryancarmody@outlook.com>
2022-11-03 21:29:12 +1030

    adding pagenation

--- graphql/queries.js
+++ graphql/queries.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 const GET_ALL_SLUGS = gql`
   query {
-    blogPosts {
+    blogPosts(pagination: { limit: 100 }) {
       data {
         attributes {
           urlSlug
@@ -14,7 +14,7 @@ const GET_ALL_SLUGS = gql`
 
 const GET_ALL_TAGS = gql`
   query {
-    tags {
+    tags(pagination: { limit: 100 }) {
       data {
         attributes {
           tagName
@@ -33,7 +33,7 @@ const GET_ALL_TAGS = gql`
 
 const GET_POSTS_FROM_TAG = gql`
   query ($tag: String!) {
-    tags(filters: { tagName: { eq: $tag } }) {
+    tags(filters: { tagName: { eq: $tag } }, pagination: { limit: 100 }) {
       data {
         attributes {
           tagName
@@ -101,7 +101,7 @@ const GET_INDIVIDUAL_POST = gql`
 
 const GET_LATEST_POSTS = gql`
   query ($type: String!) {
-    blogPosts(filters: { type: { eq: $type } }) {
+    blogPosts(filters: { type: { eq: $type } }, pagination: { limit: 100 }) {
       data {
         attributes {
           heading
